d20a70ec47a365db94b1a562349b2d21b41cece7
j-hewett <jonah.hewett1@gmail.com>
2026-06-16 01:47:46 +0100

    Move GTextEdit implementation to header, remove cpp

--- GTextEdit.h
+++ GTextEdit.h
@@ -2,16 +2,33 @@
 #define G_EDITOR_H
 
 #include <QTextEdit>
+#include <QEvent>
+#include <QWheelEvent>
 
 class GTextEdit : public QTextEdit
 {
     Q_OBJECT
 
 public:
-    explicit GTextEdit(QWidget* parent = nullptr);
-    ~GTextEdit();
+    explicit GTextEdit(QWidget* parent = nullptr) : QTextEdit(parent) {}
+    ~GTextEdit() override = default;
 private:
-    void wheelEvent(QWheelEvent* event) override;
-};
+
+    // Handle gesture zooming event
+    void wheelEvent(QWheelEvent* event) override
+    {
+        if (event->modifiers() & Qt::ControlModifier) {
+            const int dy = event->angleDelta().y() != 0 ? event->angleDelta().y()
+                                                    : event->pixelDelta().y();
+
+            if (dy > 0) zoomIn();
+            else if (dy < 0) zoomOut();
+
+            event->accept();
+            return;
+        }
+        QTextEdit::wheelEvent(event); // base class handles normal scrolling
+        }
+    };
 
 #endif // G_EDITOR_H
